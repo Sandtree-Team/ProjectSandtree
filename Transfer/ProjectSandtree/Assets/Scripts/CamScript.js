52c3335c8e644e118d6e4d7c39101b5d0cb18bd7
NWin95 <winsor.nick.cs@gmail.com>
2014-12-12 12:26:42 -0600

    Whole Project 12/12/14
    wasnt sure where to put any of this :) just threw it in transfer

---
+++ Transfer/ProjectSandtree/Assets/Scripts/CamScript.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var playerMovement	: PlayerMovement;
+
+var targString	: String;
+var followTarg	: Transform;
+var rayHit		: RaycastHit;
+var rayMask		: LayerMask;
+var camera1		: Camera;
+var pointVector	: Vector3;
+
+function Start ()
+{
+	followTarg	= GameObject.Find (targString).transform;
+	playerMovement	= followTarg.GetComponent (PlayerMovement);
+}
+
+function Update ()
+{
+	transform.position = followTarg.position;
+	
+	var ray	= camera1.ScreenPointToRay (Input.mousePosition);
+	if (Physics.Raycast (ray, rayHit, Mathf.Infinity, rayMask))
+	{
+		pointVector	= rayHit.point;
+		playerMovement.lookPoint	= pointVector;
+	}
+/*				//Just For Testing Purposes
+
+	if (Input.GetKey (KeyCode.E))
+	{	transform.eulerAngles.y += (60 * Time.deltaTime);	}
+	if (Input.GetKey (KeyCode.Q))
+	{	transform.eulerAngles.y -= (60 * Time.deltaTime);	}
+*/
+}
