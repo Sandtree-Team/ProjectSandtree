7e1d89a684f83fdf80bb1353510220a7b41e3ac7
NWin95 <winsor.nick.cs@gmail.com>
2014-12-28 11:03:17 -0600

    Made sky changing script
    mainly so we can work in default lighting, but also so different themes
    are easier to work with

--- Sandtree/Game/Sandtree/Assets/SkyScript.js
+++ Sandtree/Game/Sandtree/Assets/SkyScript.js
@@ -1,6 +1,47 @@
 ﻿#pragma strict
 
+var skySetMode	: int;
+
+var sky		: MeshRenderer;
+var sun		: Light;
+var reverse	: Light;
+var low		: Light;
+
+var sk1	: Color;
+var su1	: Color;
+var re1	: Color;
+var lo1	: Color;
+
+var sk2	: Color;
+var su2	: Color;
+var re2	: Color;
+var lo2	: Color;
+
 function Start ()
 {
-	gameObject.GetComponent (MeshRenderer).enabled	= true;
+	SkySet ();
+}
+
+function SkySet ()
+{
+	sky.enabled	= true;
+	switch (skySetMode)
+	{
+		case (0):
+			sky.enabled	= false;
+			sun.color	= Color.white;
+			break;
+		case (1):
+			sky.material.color	= sk1;
+			sun.color			= su1;
+			reverse.color		= re1;
+			low.color			= lo1;
+			break;
+		case (2):
+			sky.material.color	= sk2;
+			sun.color			= su2;
+			reverse.color		= re2;
+			low.color			= lo2;
+			break;
+	}
 }
