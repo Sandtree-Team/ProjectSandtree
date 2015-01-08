274f5cd7dc9ddaa88f35d2bafda774e8f5418f6a
NWin95 <winsor.nick.cs@gmail.com>
2015-01-08 15:08:55 -0600

    Start of patrol
    had to stop working on it

---
+++ Sandtree/Game/Sandtree/Assets/EnemyPatrolScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrolScript : MonoBehaviour {
+
+	public Transform [] waypoints;
+	public float [] pauses;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		Debug.Log (col.gameObject.tag);
+		if (col.gameObject.tag == "PatrolObject")
+		{
+			PatrolObjectScript informer;
+			informer = col.gameObject.GetComponent <PatrolObjectScript>();
+
+			waypoints = informer.waypoints;
+			pauses = informer.pauses;
+		}
+	}
+}
---
+++ Sandtree/Game/Sandtree/Assets/PatrolObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolObjectScript : MonoBehaviour {
+
+	public Transform [] waypoints;
+	public float [] pauses;
+
+	void Start ()
+	{
+		foreach (Transform waypoint in waypoints)
+		{
+			waypoint.renderer.enabled = false;
+		}
+	}
+}
